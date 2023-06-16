fa51cef9c35bf0cf6f8eb245198297f438a8e169
Rafael Santos <104213060+rafaaels2@users.noreply.github.com>
2023-06-16 12:31:06 -0300

    [ADD] Todas as condições de existência

--- semana1/Triangulo.php
+++ semana1/Triangulo.php
@@ -1,15 +1,31 @@
 <?php
-	function modulo (int $num): int {
+	// calcula o modulo de um numero
+	function modulo ($num) {
 		if ($num < 0) {
 			return $num * -1;
+		}
 		
 		return $num;
 	}
 	
-	function Triangulo (int $a, int $b, int $c): bool {
-		if ( !(($a > modulo ($b - $c)) && ($a > ($b + $c))) ) {
-			echo "Não é um triângulo válido";
+	// verifica se o triangulo eh possivel
+	function triangulo ($a, $b, $c) {
+		if ( !(($a > modulo ($b - $c)) && ($a < ($b + $c))) ) {
+			echo "Não é um triângulo possível\n";
 			return false;
 		}
+
+		if ( !(($b > modulo ($c - $a)) && ($b < ($c + $a))) ) {
+			echo "Não é um triângulo possível\n";
+			return false;
+		}
+
+		if ( !(($c > modulo ($a - $b)) && ($c < ($b + $a))) ) {
+			echo "Não é um triângulo possível\n";
+			return false;
+		}
+
+		echo "É um triângulo possível\n";
+		return true;
 	}
 ?>
